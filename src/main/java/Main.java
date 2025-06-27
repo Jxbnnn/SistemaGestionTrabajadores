f86ecc43485c010bb7a45d8cf7ba10f0009d5cad
benjamin <benjaminurrea14@gmail.com>
2025-06-27 18:14:02 -0400

    Commit del Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,8 @@
+import vista.VentanaTrabajador;
+
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== Sistema de Gestión de Trabajadores ===");
+        VentanaTrabajador.mostrarMenu();
+    }
 }
