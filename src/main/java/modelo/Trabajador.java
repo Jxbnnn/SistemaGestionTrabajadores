081c26d1c704e59885063fc630d6d0e695620f7c
benjamin <benjaminurrea14@gmail.com>
2025-06-27 17:52:42 -0400

    Avance Trabajador.java

--- src/main/java/modelo/Trabajador.java
+++ src/main/java/modelo/Trabajador.java
@@ -1,4 +1,13 @@
 package modelo;
 
-public class Trabajador {
+public class Trabajador extends Persona {
+    private String isapre;
+    private String afp;
+
+    public Trabajador (String nombre, String apellido, String rut, String isapre, String afp) {
+        super(nombre, apellido, rut);
+        this.afp = afp;
+    }
+
+
 }
