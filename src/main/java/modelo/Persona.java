3f2a16760ba3e59d85dddefc2b4f942d83938281
benjamin <benjaminurrea14@gmail.com>
2025-06-27 17:45:08 -0400

    Avance Persona.java

--- src/main/java/modelo/Persona.java
+++ src/main/java/modelo/Persona.java
@@ -1,17 +1,13 @@
 package modelo;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Persona {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    private String nombre;
+    private String apellido;
+    private String rut;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public Persona(String nombre, String apellido, String rut) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.rut = rut;
     }
 }
