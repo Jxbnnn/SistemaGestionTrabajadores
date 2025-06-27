56cd7366c80689feec7e3fa88aae67938b967ff4
benjamin <benjaminurrea14@gmail.com>
2025-06-27 18:05:48 -0400

    Avance ventana trabajador

--- src/main/java/vista/VentanaTrabajador.java
+++ src/main/java/vista/VentanaTrabajador.java
@@ -1,4 +1,27 @@
 package vista;
 
+import modelo.Trabajador;
+import java.util.Scanner;
+
 public class VentanaTrabajador {
+    public static void mostrarMenu() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("--- Registro de Trabajador ---");
+
+        System.out.print("Nombre: ");
+        String nombre = scanner.nextLine();
+
+        System.out.print("Apellido: ");
+        String apellido = scanner.nextLine();
+
+        System.out.print("RUT: ");
+        String rut = scanner.nextLine();
+
+        System.out.print("Isapre: ");
+        String isapre = scanner.nextLine();
+
+        System.out.print("AFP: ");
+        String afp = scanner.nextLine();
+
+    }
 }
