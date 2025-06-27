763380b014a1f795e7e27c39d5ded67195d57123
benjamin <benjaminurrea14@gmail.com>
2025-06-27 18:00:12 -0400

    Avance Trabajador.java

--- src/main/java/modelo/Trabajador.java
+++ src/main/java/modelo/Trabajador.java
@@ -8,6 +8,16 @@ public class Trabajador extends Persona {
         super(nombre, apellido, rut);
         this.afp = afp;
     }
-
-
+    public String getIsapre() {
+        return isapre;
+    }
+    public void setIsapre(String isapre) {
+        this.isapre = isapre;
+    }
+    public String getAfp() {
+        return afp;
+    }
+    public void setAfp(String afp) {
+        this.afp = afp;
+    }
 }
